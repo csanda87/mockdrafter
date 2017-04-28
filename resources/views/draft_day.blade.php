62124bdb4e18d3185b32c4303ad6ee546c9a26fc
Chad Sanda <csanda@hotmail.com>
2017-04-27 19:37:20 -0500

    updates

--- resources/views/draft_day.blade.php
+++ resources/views/draft_day.blade.php
@@ -3,9 +3,118 @@
 @section('content')
 <div class="container">
 	<div class="row">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-2">
+			Dirt: 
+		</div>
+		<div class="col-sm-2">
+			Berg: 
+		</div>
+		<div class="col-sm-2">
+			Nickel:
+		</div>
+		<div class="col-sm-2">
+			R3lic:
+		</div>
+		<div class="col-sm-2">
+			Ronko: 
+		</div>
+	</div>
+	<div class="row">
 		<div class="col-sm-2">
 			<h3>Official</h3>
 			<div class="list-group">
+				<div class="list-group-item">
+					1 - CLE<br>
+					Myles Garrett
+				</div>
+				<div class="list-group-item">
+					2 - CHI (trade from <br>
+					Mitch Trubisky
+				</div>
+				<div class="list-group-item">
+					3 - SF (trade from CHI)<br>
+					Solomon Thomas
+				</div>
+				<div class="list-group-item">
+					4 - JAX (trade from CHI)<br>
+					Solomon Thomas
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
+				<div class="list-group-item">
+					
+				</div>
 				<div class="list-group-item">
 					
 				</div>
